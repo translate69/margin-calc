2d653a59e9c20b2ef17f4f6ebefd5995ad585300
translate69 <translate69@users.noreply.github.com>
2026-08-18 03:31:35 +0800

    chore: 一次性用品标签由围裙改为桌布

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -679,7 +679,7 @@ export default function HotpotCalculator() {
             />
           </div>
           <div className="row">
-            <label>一次性用品（围裙/纸巾）</label>
+            <label>一次性用品（桌布/纸巾）</label>
             <input
               type="number"
               step="any"
